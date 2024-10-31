c0bc81c7842b33c2b21d3aa45aac85ef1c4683e3
EthxnB <ethxnbell@gmail.com>
2024-10-31 18:22:01 -0400

    First Commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react'
+import Nav from './components/Nav.jsx'
+import Footer from './components/Footer.jsx';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Home from './pages/Home.jsx';
+import Books from './pages/Books.jsx';
+import { books } from "./data";
+import BookInfo from './pages/BookInfo.jsx';
+import Cart from './pages/Cart.jsx';
+
+function App() {
+  const [cart, setCart] = useState([])
+
+function addToCart(book) {
+  setCart([...cart, book])
+}
+
+useEffect(() => {
+  console.log(cart);
+}, [cart])
+
+  return (
+    <Router>
+    <div className="App">
+      <Nav />
+      <Route path="/" exact component={Home} />
+      <Route path="/books" exact render={() => <Books books={books} />} />
+      <Route path="/books/:id" render={() => <BookInfo books={books} addToCart={addToCart} />} />
+      <Route path="/cart" render={() => <Cart books={books} />} />
+      <Footer />
+    </div>
+    </Router>
+  );
+}
+
+export default App;
